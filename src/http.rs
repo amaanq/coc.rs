25677e65545218c9dda02a436891fbf3fadfd858
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-06-11 21:33:48 +0100

    couple changes - made a new method inside `Client`, which returns a generic `Result<T, ApiError>` this will allow us to be more flexible and less repeated code.
    - A constant variable as base url.
    
    - To make a new endpoint, copy `get_player()` for now lol

--- src/http.rs
+++ src/http.rs
@@ -5,6 +5,7 @@ use serde::{Serialize, Deserialize};
 extern crate reqwest;
 
 use reqwest::header::{HeaderMap, HeaderValue};
+use serde::de::DeserializeOwned;
 use crate::entites::_player::*;
 
 #[derive(Debug)]
@@ -18,6 +19,8 @@ pub enum ApiError {
     Api(reqwest::StatusCode),
 }
 
+const BASE_URL: &str = "https://api.clashofclans.com/v1";
+
 impl Clinet {
     pub fn new(token: String) -> Self { Self { token } }
 
@@ -30,9 +33,25 @@ impl Clinet {
             .headers(headers);
         Ok(res)
     }
+    pub async fn get_clan(){
+
+    }
     pub async fn get_player(&self, tag: String) -> Result<Player, ApiError> {
-        let url = format!("https://api.clashofclans.com/v1/players/{}", self.format_tag(tag));
+        let url = format!("{}/players/{}", BASE_URL,  self.format_tag(tag));
         // let player = self.get(url).await?.send().await?.json().await?;
+        self.parse_json::<Player>(url).await
+    }
+    //It should return a String of "%23+tag"
+    fn format_tag(&self, tag: String) -> String {
+        return if tag[0..1].eq_ignore_ascii_case("#") {
+            tag.replace("#", "%23")
+        } else {
+            format!("%23{}", tag)
+        };
+    }
+
+    async fn parse_json<T: DeserializeOwned> (&self, url: String) -> Result<T, ApiError> {
+        //let url = format!("https://api.clashofclans.com/v1/players/{}", self.format_tag(tag));
 
         let player = self.get(url).await;
         match player {
@@ -41,7 +60,7 @@ impl Clinet {
                     Ok(res) => {
                         match res.status() {
                             reqwest::StatusCode::OK => {
-                                Ok(res.json().await.expect("Json Error 1"))
+                                Ok(res.json().await.expect("Unexpected json response from the API, cannot parse json"))
                             },
                             _ => {
                                 Err(ApiError::Api(res.status()))
@@ -53,14 +72,5 @@ impl Clinet {
             }
             Err(e) => return Err(ApiError::Request(e)),
         }
-    }
-
-    //It should return a String of "%23+tag"
-    fn format_tag(&self, tag: String) -> String {
-        return if tag[0..1].eq_ignore_ascii_case("#") {
-            tag.replace("#", "%23")
-        } else {
-            format!("%23{}", tag)
-        };
     }
 }
