9d0142d812f2a67faa55dbcfbb3ea183448d2096
Sahilpanchal2903 <sahilpanchal2903@gmail.com>
2022-06-13 14:11:56 +0100

    Removed, `BadgeUrls` from all and moved to a new file. And added more getters

--- src/entites/current_war.rs
+++ src/entites/current_war.rs
@@ -1,4 +1,5 @@
 use serde::{ Serialize, Deserialize };
+use crate::entites::badge_urls::BadgeUrls;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct War {
@@ -54,17 +55,6 @@ pub struct WarClan {
     members: Vec<Member>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct BadgeUrls {
-    #[serde(rename = "small")]
-    small: String,
-
-    #[serde(rename = "large")]
-    large: String,
-
-    #[serde(rename = "medium")]
-    medium: String,
-}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Member {
@@ -109,4 +99,103 @@ pub struct Attack {
 
     #[serde(rename = "duration")]
     duration: i32,
+}
+
+impl War {
+    pub fn state(&self) -> &str {
+        &self.state
+    }
+    pub fn team_size(&self) -> i32 {
+        self.team_size
+    }
+    pub fn attacks_per_member(&self) -> i8 {
+        self.attacks_per_member
+    }
+    pub fn preparation_start_time(&self) -> &str {
+        &self.preparation_start_time
+    }
+    pub fn start_time(&self) -> &str {
+        &self.start_time
+    }
+    pub fn end_time(&self) -> &str {
+        &self.end_time
+    }
+    pub fn clan(&self) -> &WarClan {
+        &self.clan
+    }
+    pub fn opponent(&self) -> &WarClan {
+        &self.opponent
+    }
+}
+
+impl WarClan {
+    pub fn tag(&self) -> &str {
+        &self.tag
+    }
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn badge_urls(&self) -> &BadgeUrls {
+        &self.badge_urls
+    }
+    pub fn clan_level(&self) -> i8 {
+        self.clan_level
+    }
+    pub fn attacks(&self) -> i32 {
+        self.attacks
+    }
+    pub fn stars(&self) -> i32 {
+        self.stars
+    }
+    pub fn destruction_percentage(&self) -> f64 {
+        self.destruction_percentage
+    }
+    pub fn members(&self) -> &Vec<Member> {
+        &self.members
+    }
+}
+
+impl Member {
+    pub fn tag(&self) -> &str {
+        &self.tag
+    }
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+    pub fn townhall_level(&self) -> i8 {
+        self.townhall_level
+    }
+    pub fn map_position(&self) -> i32 {
+        self.map_position
+    }
+    pub fn attacks(&self) -> &Vec<Attack> {
+        &self.attacks
+    }
+    pub fn opponent_attacks(&self) -> i32 {
+        self.opponent_attacks
+    }
+    pub fn best_opponent_attack(&self) -> &Attack {
+        &self.best_opponent_attack
+    }
+}
+
+impl Attack {
+    pub fn attacker_tag(&self) -> &str {
+        &self.attacker_tag
+    }
+    pub fn defender_tag(&self) -> &str {
+        &self.defender_tag
+    }
+    pub fn stars(&self) -> i32 {
+        self.stars
+    }
+    pub fn destruction_percentage(&self) -> f32 {
+        self.destruction_percentage
+    }
+    pub fn order(&self) -> i32 {
+        self.order
+    }
+    pub fn duration(&self) -> i32 {
+        self.duration
+    }
 }
