9c8bc90575d5313208df3516ba6485d0ff84662f
sahil <sahilpanchal2903@gmail.com>
2022-07-12 23:19:16 +0100

    YEP

--- src/dev.rs
+++ src/dev.rs
@@ -1,8 +1,35 @@
+use reqwest::{Error, RequestBuilder, Response};
+use serde::{ Deserialize, Serialize };
+use crate::api::Client;
+
 // manage a session
 pub const BASE_DEV_URL: &str = "https://developer.clashofclans.com/api";
 const IP_URL: &str = "https://api.ipify.org";
 
-pub fn login(username: String, password: String){
-    reqwest::Client::new().post(format!("{}/login",BASE_DEV_URL));
+pub async fn login(email: String, password: String) -> reqwest::Client {
+    let client = reqwest::Client::builder()
+        .cookie_store(true)
+        .build()
+        .unwrap();
+    client.post(format!("{}/login", BASE_DEV_URL))
+        .json::<Creds>(&Creds {
+            email,
+            password,
+        })
+        .send()
+        .await.expect("TODO: panic message");
+    client
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Creds {
+    pub email: String,
+    pub password: String,
+}
+
+async fn get_ip() -> Result<String, reqwest::Error> {
+    let res = reqwest::Client::new().get(IP_URL).send().await?;
+    let ip = res.text().await?;
+    Ok(ip)
 }
 
--- src/test.rs
+++ src/test.rs
@@ -13,8 +13,9 @@ mod tests {
 
         {
             let client = api::Client::new(
-                std::env::var("COC_TOKEN").unwrap(),
-            );
+                "email".to_string(),
+                "password".to_string()
+            ).await;
 
             let tag = "#2PP".to_string();
             match client.get_player(tag).await {
@@ -31,140 +32,140 @@ mod tests {
         println!("Elapsed: {:.2?}", elapsed);
     }
 
-    #[tokio::test]
-    async fn test_player() {
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-
-        let tag = "#2PP".to_string();
-        match client.get_player(tag).await {
-            Ok(body) => {
-                println!("{:?}", body);
-            }
-            Err(e) => match e {
-                api::ApiError::Request(e) => {
-                    println!("Request - {:?}", e);
-                    assert!(true);
-                }
-                api::ApiError::Api(e) => {
-                    if e == reqwest::StatusCode::NOT_FOUND {
-                        println!("Not found")
-                    } else {
-                        println!("Some other variant {}", e.as_u16());
-                    }
-                    assert!(false);
-                }
-            },
-        }
-    }
-
-    #[tokio::test]
-    async fn test_clan(){
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-        let tag = "#2pp".to_string();
-
-        let x = client.get_clan(tag).await.expect("Unable to get clan");
-
-        println!("{:?}", x);
-        assert!(true)
-    }
-
-    #[tokio::test]
-    async fn test_current_war(){
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-        let tag = "r8j".to_string();
-
-        let x = client.get_current_war(tag);
-        match x.await {
-            Ok(body) => {
-                println!("{:?}", body);
-            }
-            Err(e) => match e {
-                api::ApiError::Request(e) => {
-                    println!("Request - {:?}", e);
-                    assert!(true)
-                }
-                api::ApiError::Api(e) => {
-                    if e == reqwest::StatusCode::NOT_FOUND {
-                        println!("Not found")
-                    }else {
-                        println!("Some other variant {}", e.as_u16())
-                    }
-                    assert!(false)
-                }
-            },
-        }
-    }
-
-    #[tokio::test]
-    async fn test_player_token(){
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-        let tag = "#CVJLQOLR".to_string();
-        let token = "".to_string();
-
-        let x = client.get_verified_player(tag, token);
-        match x.await {
-            Ok(body) => {
-                println!("{:?}", body);
-            }
-            Err(e) => match e {
-                api::ApiError::Request(e) => {
-                    println!("Request - {:?}", e);
-                }
-                api::ApiError::Api(e) => {
-                    if e == reqwest::StatusCode::NOT_FOUND {
-                        println!("Not found");
-                        assert!(true)
-                    }else {
-                        println!("Some other variant {}", e.as_u16())
-                    }
-                    assert!(false)
-                }
-            },
-        }
-    }
-
-    #[tokio::test]
-    async fn test_clan_members(){
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-
-        let x = client.get_clan_members(
-            "2pp".to_string(),
-            ConfigForRezponse {
-                limit: None,
-                time: Some(api::Time::Before("eyJwb3MiOjF9".to_string()))
-            }
-        ).await.unwrap();
-
-        println!("{:?}", x)
-
-    }
-
-    #[tokio::test]
-    async fn test_clan_warlog(){
-        let client = api::Client::new(
-            std::env::var("COC_TOKEN").unwrap(),
-        );
-
-        let x = client.get_clan_warlog(
-            "r8j".to_string(),
-            ConfigForRezponse {
-                limit: None,
-                time: None
-            }
-        ).await.unwrap();
-
-        println!("{:?}", x)
-
-    }
+    // #[tokio::test]
+    // async fn test_player() {
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //
+    //     let tag = "#2PP".to_string();
+    //     match client.get_player(tag).await {
+    //         Ok(body) => {
+    //             println!("{:?}", body);
+    //         }
+    //         Err(e) => match e {
+    //             api::ApiError::Request(e) => {
+    //                 println!("Request - {:?}", e);
+    //                 assert!(true);
+    //             }
+    //             api::ApiError::Api(e) => {
+    //                 if e == reqwest::StatusCode::NOT_FOUND {
+    //                     println!("Not found")
+    //                 } else {
+    //                     println!("Some other variant {}", e.as_u16());
+    //                 }
+    //                 assert!(false);
+    //             }
+    //         },
+    //     }
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_clan(){
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //     let tag = "#2pp".to_string();
+    //
+    //     let x = client.get_clan(tag).await.expect("Unable to get clan");
+    //
+    //     println!("{:?}", x);
+    //     assert!(true)
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_current_war(){
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //     let tag = "r8j".to_string();
+    //
+    //     let x = client.get_current_war(tag);
+    //     match x.await {
+    //         Ok(body) => {
+    //             println!("{:?}", body);
+    //         }
+    //         Err(e) => match e {
+    //             api::ApiError::Request(e) => {
+    //                 println!("Request - {:?}", e);
+    //                 assert!(true)
+    //             }
+    //             api::ApiError::Api(e) => {
+    //                 if e == reqwest::StatusCode::NOT_FOUND {
+    //                     println!("Not found")
+    //                 }else {
+    //                     println!("Some other variant {}", e.as_u16())
+    //                 }
+    //                 assert!(false)
+    //             }
+    //         },
+    //     }
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_player_token(){
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //     let tag = "#CVJLQOLR".to_string();
+    //     let token = "".to_string();
+    //
+    //     let x = client.get_verified_player(tag, token);
+    //     match x.await {
+    //         Ok(body) => {
+    //             println!("{:?}", body);
+    //         }
+    //         Err(e) => match e {
+    //             api::ApiError::Request(e) => {
+    //                 println!("Request - {:?}", e);
+    //             }
+    //             api::ApiError::Api(e) => {
+    //                 if e == reqwest::StatusCode::NOT_FOUND {
+    //                     println!("Not found");
+    //                     assert!(true)
+    //                 }else {
+    //                     println!("Some other variant {}", e.as_u16())
+    //                 }
+    //                 assert!(false)
+    //             }
+    //         },
+    //     }
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_clan_members(){
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //
+    //     let x = client.get_clan_members(
+    //         "2pp".to_string(),
+    //         ConfigForRezponse {
+    //             limit: None,
+    //             time: Some(api::Time::Before("eyJwb3MiOjF9".to_string()))
+    //         }
+    //     ).await.unwrap();
+    //
+    //     println!("{:?}", x)
+    //
+    // }
+    //
+    // #[tokio::test]
+    // async fn test_clan_warlog(){
+    //     let client = api::Client::new(
+    //         std::env::var("COC_TOKEN").unwrap(),
+    //     );
+    //
+    //     let x = client.get_clan_warlog(
+    //         "r8j".to_string(),
+    //         ConfigForRezponse {
+    //             limit: None,
+    //             time: None
+    //         }
+    //     ).await.unwrap();
+    //
+    //     println!("{:?}", x)
+    //
+    // }
 
 }
