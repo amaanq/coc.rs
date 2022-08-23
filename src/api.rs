c6360436396d88a3021dfd250bd7e3df16434241
Amaan Qureshi <amaanq12@gmail.com>
2022-08-22 23:23:20 -0400

    ok

--- src/api.rs
+++ src/api.rs
@@ -13,23 +13,22 @@ use crate::models::current_war::War;
 use crate::models::gold_pass::GoldPass;
 use crate::models::player::{Player, PlayerToken};
 
+use crate::models::war_log::WarLog;
 use reqwest::header::{HeaderMap, HeaderValue};
 use reqwest::RequestBuilder;
 use serde::de::DeserializeOwned;
-use crate::models::war_log::WarLog;
 
-use std::sync::Mutex;
-use lazy_static::lazy_static;
 use crate::dev;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
 
 use futures;
-
 
 #[macro_use]
 #[derive(Debug)]
 pub struct Client {
     username: String,
-    password: String
+    password: String,
 }
 
 #[derive(Debug)]
@@ -37,6 +36,7 @@ pub enum ApiError {
     Request(reqwest::Error),
     Api(reqwest::StatusCode),
 }
+
 lazy_static! {
     static ref TOKEN_LIST: Mutex<Vec<String>> = Mutex::new(vec![]);
     static ref s_Client: reqwest::Client = reqwest::Client::new();
@@ -46,11 +46,9 @@ const BASE_URL: &str = "https://api.clashofclans.com/v1";
 
 impl Client {
     pub async fn new(username: String, password: String) -> Self {
-        let client = Self {
-            username,
-            password,
-        };
-        let _ = futures::future::join_all(client.init());
+        let client = Self { username, password };
+        client.init().await;
+        // let _ = futures::future::join_all(client.init());
         client
     }
 
@@ -65,23 +63,18 @@ impl Client {
     }
 
     fn get(&self, url: String) -> Result<reqwest::RequestBuilder, reqwest::Error> {
-        let res = s_Client
-            .get(url)
-            .bearer_auth(&self.cycle());
+        let res = s_Client.get(url).bearer_auth(&self.cycle());
         Ok(res)
     }
 
     fn post(&self, url: String, body: String) -> Result<reqwest::RequestBuilder, reqwest::Error> {
-        let res = s_Client
-            .post(url)
-            .bearer_auth(&self.cycle())
-            .body(body);
+        let res = s_Client.post(url).bearer_auth(&self.cycle()).body(body);
         Ok(res)
     }
 
     ///                                                            ///
     ///                                                            ///
-    /// --------------------------END POINTS-----------------------///
+    /// --------------------------ENDPOINTS----------------------- ///
     ///                                                            ///
     ///                                                            ///
     pub async fn get_clan(&self, tag: String) -> Result<Clan, ApiError> {
@@ -114,14 +107,28 @@ impl Client {
         self.parse_json::<PlayerToken>(self.post(url, token)).await
     }
 
-    pub async fn get_clan_members(&self, tag: String, config: ConfigForRezponse) -> Result<Rezponse<ClanMember>, ApiError> {
-        let mut url = format!("https://api.clashofclans.com/v1/clans/{}/members", self.format_tag(tag));
+    pub async fn get_clan_members(
+        &self,
+        tag: String,
+        config: ConfigForRezponse,
+    ) -> Result<Rezponse<ClanMember>, ApiError> {
+        let mut url = format!(
+            "https://api.clashofclans.com/v1/clans/{}/members",
+            self.format_tag(tag)
+        );
         url = self.get_cursor_url(url, config);
         self.parse_json::<Rezponse<ClanMember>>(self.get(url)).await
     }
 
-    pub async fn get_clan_warlog(&self, tag: String, config: ConfigForRezponse) -> Result<Rezponse<WarLog>, ApiError> {
-        let mut url = format!("https://api.clashofclans.com/v1/clans/{}/warlog", self.format_tag(tag));
+    pub async fn get_clan_warlog(
+        &self,
+        tag: String,
+        config: ConfigForRezponse,
+    ) -> Result<Rezponse<WarLog>, ApiError> {
+        let mut url = format!(
+            "https://api.clashofclans.com/v1/clans/{}/warlog",
+            self.format_tag(tag)
+        );
         url = self.get_cursor_url(url, config);
         self.parse_json::<Rezponse<WarLog>>(self.get(url)).await
     }
@@ -137,33 +144,30 @@ impl Client {
             Some(s) => {
                 url = format!("{}?limit={}", url, s);
                 match config.time {
-                    None => {
-                        url
-                    }
-                    Some(s1) => {
-                        match s1 {
-                            Time::After(a) => { format!("{}&after={}", url, a) }
-                            Time::Before(b) => { format!("{}&before={}", url, b) }
+                    None => url,
+                    Some(s1) => match s1 {
+                        Time::After(a) => {
+                            format!("{}&after={}", url, a)
                         }
-                    }
+                        Time::Before(b) => {
+                            format!("{}&before={}", url, b)
+                        }
+                    },
                 }
             }
-            None => {
-                match config.time {
-                    None => {
-                        url
+            None => match config.time {
+                None => url,
+                Some(s) => match s {
+                    Time::After(a) => {
+                        format!("{}?after={}", url, a)
                     }
-                    Some(s) => {
-                        match s {
-                            Time::After(a) => { format!("{}?after={}", url, a) }
-                            Time::Before(b) => { format!("{}?before={}", url, b) }
-                        }
+                    Time::Before(b) => {
+                        format!("{}?before={}", url, b)
                     }
-                }
-            }
+                },
+            },
         }
     }
-
 
     fn format_tag(&self, tag: String) -> String {
         return if tag[0..1].contains("#") {
@@ -174,20 +178,17 @@ impl Client {
     }
 
     fn is_valid_tag(&self, tag: String) -> bool {
-        Regex::new("^#[PYLQGRJCUV0289]+$").unwrap()
-            .is_match(
-                tag.to_uppercase()
-                    .replace("O", "0")
-                    .as_str()
-            )
+        Regex::new("^#[PYLQGRJCUV0289]+$")
+            .unwrap()
+            .is_match(tag.to_uppercase().replace("O", "0").as_str())
     }
 
     fn fix_tag(&self, tag: String) -> String {
         let re = Regex::new("[^A-Z0-9]+").unwrap();
         "#".to_owned()
             + &re
-            .replace_all(tag.to_uppercase().as_str(), "")
-            .replace("O", "0")
+                .replace_all(tag.to_uppercase().as_str(), "")
+                .replace("O", "0")
     }
 
     async fn parse_json<T: DeserializeOwned>(
@@ -198,7 +199,7 @@ impl Client {
             Ok(rb) => match rb.send().await {
                 Ok(res) => match res.status() {
                     reqwest::StatusCode::OK => {
-                        let t = res
+                        let t: String = res
                             .json()
                             .await
                             .expect("Unexpected json response from the API, cannot parse json");
@@ -215,7 +216,13 @@ impl Client {
 
     fn cycle(&self) -> String {
         TOKEN_LIST.lock().unwrap().rotate_left(1);
-        TOKEN_LIST.lock().unwrap().get(0).unwrap().to_string().clone()
+        TOKEN_LIST
+            .lock()
+            .unwrap()
+            .get(0)
+            .unwrap()
+            .to_string()
+            .clone()
     }
 }
 
